e91565265ea3f932bffeac73fec30f1c18600f7d
Craig Condon <craig@classdojo.com>
2013-12-03 19:31:41 -0600

    added sections test

--- test/core/sections-test.js
+++ test/core/sections-test.js
@@ -14,6 +14,73 @@ describe("core/sections#", function () {
   - class reference
   - custom component
   - createFragment set on view
+  - decorate on initialize
   */
+
+
+  it("can define a section with a class", function (next) {
+
+    var ParentView = mojo.View.extend({
+      sections: {
+        child: mojo.View
+      }
+    });
+
+    ParentView.prototype.__decorators = undefined;
+
+    var p = new ParentView({}, app);
+
+    p.render(function () {
+      expect(p.get("sections.child").constructor).to.be(mojo.View);
+      next();
+    });
+  });
+
+  /**
+   */
+
+  it("can define a section when the type is a class", function (next) {
+
+    var ParentView = mojo.View.extend({
+      sections: {
+        child: {
+          type: mojo.View,
+          message: "blah"
+        }
+      }
+    });
+
+    ParentView.prototype.__decorators = undefined;
+
+    var p = new ParentView({}, app);
+
+    p.render(function () {
+      expect(p.get("sections.child").constructor).to.be(mojo.View);
+      expect(p.get("sections.child").message).to.be("blah");
+      next();
+    });
+  });
+
+  /**
+   */
+
+  it("can define a section when the type is a registered component", function (next) {
+    var ParentView = mojo.View.extend({
+      sections: {
+        child: {
+          type: "basic"
+        }
+      }
+    });
+
+    ParentView.prototype.__decorators = undefined;
+
+    var p = new ParentView({}, app);
+
+    p.render(function () {
+      expect(p.get("sections.child").constructor).to.be(mojo.View);
+      next();
+    });
+  });
 
 }); 
