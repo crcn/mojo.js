7f9af6595abda4a232e0999b82df87f254371daf
Craig Condon <craig@classdojo.com>
2013-12-03 18:31:00 -0600

    make test more redundant

--- test/core/parent-child-test.js
+++ test/core/parent-child-test.js
@@ -45,20 +45,32 @@ describe("parent/child#", function () {
 
   it("properly inherits application", function () {
 
-    var parent = app.createView("basic"),
-    child      = app2.createView("basic"),
-    subChild   = app2.createView("basic");
-
+    var parent  = app.createView("basic"),
+    child       = app2.createView("basic"),
+    subChild    = app2.createView("basic"),
+    subSubChild = app2.createView("basic");
 
     child.setChild("someChild", subChild);
+    subChild.setChild("subChild", subSubChild);
 
     expect(child.application).to.be(app2);
+    expect(child.models).to.be(app2.models);
     expect(subChild.application).to.be(app2);
+    expect(subChild.models).to.be(app2.models);
+    expect(subSubChild.application).to.be(app2);
+    expect(subSubChild.models).to.be(app2.models);
 
     parent.setChild("someChild", child);
 
     expect(child.application).to.be(app);
+    expect(child.models).to.be(app.models);
     expect(subChild.application).to.be(app);
+    expect(subChild.models).to.be(app.models);
+    expect(subSubChild.application).to.be(app);
+    expect(subSubChild.models).to.be(app.models);
+
+    
+
 
   });
 
